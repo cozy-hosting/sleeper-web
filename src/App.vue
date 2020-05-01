f98b323cc2d9969b326e72c1d865e043dc69313e
Jens Döllmann <jens@doellmann.com>
2020-05-01 19:41:18 +0200

    Changed

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png" />
-    <div>
-      <p>
-        Test
-      </p>
-      <a-button type="primary">Primary</a-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />-->
-    <router-view></router-view> 
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "app",
-  //components: {
-   // HelloWorld
- // }
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
