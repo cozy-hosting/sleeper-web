463c47a7db74aa6b77b60b5ddf0316ca8b5e0f61
Jens Döllmann <jens@doellmann.com>
2020-05-06 16:24:44 +0200

    Layout change

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,35 @@
 <template>
-    <a-layout style="position: relative; height: 100%">
-        <a-layout-header class="header">
-            <router-link to="/"><div class="logo">Cozy</div></router-link>
-            <a-menu theme="dark" mode="horizontal" @select="onMenuItemSelected"
-                    :selectedKeys="selectedKeys"
-                    :style="{ lineHeight: '64px' }">
-                <a-menu-item key="/deployments">Deployments</a-menu-item>
-                <a-menu-item key="/containers">Containers</a-menu-item>
-                <a-menu-item key="/images">Images</a-menu-item>
-            </a-menu>
-        </a-layout-header>
-        <a-layout>
-            <a-layout-sider width="200" style="background: #fff">
-                <a-menu mode="inline" :selectedKeys="selectedKeys" :style="{ height: '100%', borderRight: 0 }"
-                        @select="onMenuItemSelected">
-                    <a-menu-item key="/deployments">Deployments</a-menu-item>
-                    <a-menu-item key="/containers">Containers</a-menu-item>
-                    <a-menu-item key="/images">Images</a-menu-item>
-                </a-menu>
-            </a-layout-sider>
-            <a-layout style="padding: 0 24px 24px">
-                <router-view></router-view>
-            </a-layout>
-        </a-layout>
-        <a-layout-footer style="text-align: center">
-            Cozy ©2020
-        </a-layout-footer>
-    </a-layout>
+  <div class="app">
+    <header>
+      <router-link :to="{ name: 'Home' }">Cozy</router-link>
+    </header>
+  </div>
 </template>
 
 <script lang="ts">
 import Component from "vue-class-component";
-import Vue from 'vue'
+import Vue from "vue";
 
 @Component({})
 export default class App extends Vue {
-    private currentPage: string[] = [];
+  private currentPage: string[] = [];
 
-    onMenuItemSelected(e: any) {
-        this.$router.push(e.key);
-    }
+  onMenuItemSelected(e: any) {
+    this.$router.push(e.key);
+  }
 
-    get selectedKeys() {
-        return [this.$route.path];
-    }
-};
+  get selectedKeys() {
+    return [this.$route.path];
+  }
+}
 </script>
 
 <style lang="scss">
 .logo {
-    width: 120px;
-    font-size: 36px;
-    color: white;
-    float: left;
-    line-height: 1.55;
+  width: 120px;
+  font-size: 36px;
+  color: white;
+  float: left;
+  line-height: 1.55;
 }
 </style>
