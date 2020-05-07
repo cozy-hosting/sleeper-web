d17de1c372aec21f0ae92aab8c3684cc21ed7cda
Jens Döllmann <jens@doellmann.com>
2020-05-07 09:36:10 +0200

    Rework of basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,85 @@
 <template>
   <div class="app">
-    <header>
-      <router-link :to="{ name: 'Home' }">Cozy</router-link>
+    <header class="header">
+      <router-link :to="{ name: 'Home' }" class="logo">Cozy</router-link>
     </header>
+    <nav class="sider">
+      <a-menu>
+        <a-menu-item>
+          <router-link :to="{ name: 'Deployments' }">Deployments</router-link>
+        </a-menu-item>
+        <a-menu-item>
+          <router-link :to="{ name: 'Containers' }">Container</router-link>
+        </a-menu-item>
+        <a-menu-item>
+          <router-link :to="{ name: 'Images' }">Images</router-link>
+        </a-menu-item>
+      </a-menu>
+    </nav>
+    <main class="main">
+      <router-view></router-view>
+    </main>
+    <footer class="footer">Cozy 2020</footer>
   </div>
 </template>
 
-<script lang="ts">
-import Component from "vue-class-component";
-import Vue from "vue";
-
-@Component({})
-export default class App extends Vue {
-  private currentPage: string[] = [];
-
-  onMenuItemSelected(e: any) {
-    this.$router.push(e.key);
-  }
-
-  get selectedKeys() {
-    return [this.$route.path];
-  }
-}
-</script>
+<script lang="ts"></script>
 
 <style lang="scss">
-.logo {
-  width: 120px;
-  font-size: 36px;
-  color: white;
-  float: left;
-  line-height: 1.55;
+* {
+  box-sizing: border-box;
+}
+
+.app {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: minmax(230px, 15vw) 1fr;
+  grid-template-rows: 80px 1fr 70px;
+  grid-template-areas:
+    "header header"
+    "sider main"
+    "sider footer";
+}
+
+.header {
+  grid-area: header;
+  background: #001529;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 4em;
+
+  .logo {
+    display: block;
+    font-size: 32px;
+    color: white;
+  }
+}
+
+.sider {
+  grid-area: sider;
+
+  .ant-menu {
+    height: 100%;
+    padding: 30px 0;
+
+    .ant-menu-item {
+      padding: 0 32px;
+    }
+  }
+}
+
+.main {
+  grid-area: main;
+  padding: 4em;
+}
+
+.footer {
+  grid-area: footer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 4em;
+  border-top: 1px solid #e8e8e8;
 }
 </style>
