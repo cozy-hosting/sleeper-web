c39ed471e8f25c0595b007cce19f1e43ca7e1576
Hannes Burger <hburger30@gmail.com>
2020-05-08 21:07:07 +0200

    introduce portals for sidebar, move deployment to views directory

--- src/views/Images.vue
+++ src/views/Images.vue
@@ -1,23 +1,24 @@
 <template>
-    <div>
-        <a-breadcrumb style="margin: 16px 0">
-            <a-breadcrumb-item><router-link to="/">Home</router-link></a-breadcrumb-item>
-            <a-breadcrumb-item>Images</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0 }">
-            <p>Images</p>
-        </a-layout-content>
-    </div>
+  <div>
+    <portal to="sidebar"></portal>
+    <a-breadcrumb style="margin: 16px 0">
+      <a-breadcrumb-item><router-link to="/">Home</router-link></a-breadcrumb-item>
+      <a-breadcrumb-item>Images</a-breadcrumb-item>
+    </a-breadcrumb>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0 }">
+      <p>Images</p>
+    </a-layout-content>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+    import Vue from 'vue'
+    import Component from 'vue-class-component'
 
-@Component({})
-export default class Images extends Vue {
+    @Component({})
+    export default class Images extends Vue {
 
-}
+    }
 </script>
 
 <style lang="scss">
