9413d56bd02bc741d3b2c8ffab58227ac7f0c805
tzAcee <maximleis3@gmail.com>
2020-05-16 16:51:05 +0200

    tyscan

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    "quotes": "double",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-explicit-any": "off"
