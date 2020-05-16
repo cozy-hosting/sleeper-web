ddd4f4788ab0bc2ee01813f11a28628c2cb1abce
tzAcee <maximleis3@gmail.com>
2020-05-16 17:24:19 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": ["error", "double", {"avoidEscape": true}],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-explicit-any": "off"
