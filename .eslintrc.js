ae6d807853f731a682bb0b1995a1e96d5c2f4911
tzAcee <maximleis3@gmail.com>
2020-05-16 17:30:36 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double", {"avoidEscape": true}],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "brace-style": ["error", "allman", { "allowSingle": true}],
+    "brace-style": ["error", "allman", { "allowSingleLine": true}],
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-explicit-any": "off"
   },
