a956702d235045b44defac97e58dfa7907dca2fe
Jens Döllmann <jens@doellmann.com>
2020-06-25 00:31:52 +0200

    Mixin for form submission

---
+++ src/mixins/SubmitMixin.ts
@@ -0,0 +1,28 @@
+import { Vue, Component } from "vue-property-decorator";
+import { FormModel } from "ant-design-vue";
+import { AxiosResponse } from "axios";
+
+@Component
+export class SubmitMixin extends Vue {
+  submitting = false;
+
+  mixinSubmit(
+    form: FormModel,
+    cb: () => Promise<AxiosResponse<any>>,
+    successCb: () => void
+  ) {
+    form.validate(async valid => {
+      if (valid) {
+        try {
+          this.submitting = true;
+          await cb();
+          successCb();
+        } catch (error) {
+          console.log("Submit error: " + error);
+        } finally {
+          this.submitting = false;
+        }
+      }
+    });
+  }
+}
