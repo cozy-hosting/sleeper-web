0eb4d8ed0b17ed84b9e95094ffe9501ab752949c
tzAcee <maximleis3@gmail.com>
2020-06-24 18:15:58 +0200

    Changes for ExtApi

--- src/services/ExtApiClient.ts
+++ src/services/ExtApiClient.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
 
+function getUrl()
+{
+  let depUrl = location.hostname;
+  console.log(depUrl);
+  if (depUrl.includes("web.cozy"))
+  {
+    depUrl=depUrl.replace("web", "sleeper");
+    return depUrl;
+  }
+  return "https://localhost:5001";
+}
+
 // API Client for External MicroServices [Glancers, User]
 const ExtApiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_URL ? process.env.VUE_APP_API_URL : "https://localhost:5001",
+  baseURL: process.env.VUE_APP_API_URL ? process.env.VUE_APP_API_URL : getUrl(),
   headers: {
     "X-Forwarded-Authorization": "Bearer " + localStorage.getItem("authToken")
   }
