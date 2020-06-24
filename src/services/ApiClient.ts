5ba658f7bd13a344eed2d42dce7e7001bb511490
tzAcee <maximleis3@gmail.com>
2020-06-24 16:03:31 +0200

    Creation Form Bug Fix

--- src/services/ApiClient.ts
+++ src/services/ApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.VUE_APP_API_URL ? process.env.VUE_APP_API_URL : "https://localhost:5001",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("authToken")
   }
